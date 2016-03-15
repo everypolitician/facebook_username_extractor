5842606e945989d9941988697449d60fbde814f7
Chris Mytton <chrismytton@gmail.com>
2016-03-15 12:32:54 +0100

    Working implementation

--- lib/facebook_username_extractor.rb
+++ lib/facebook_username_extractor.rb
@@ -1,5 +1,21 @@
 require 'facebook_username_extractor/version'
 
 module FacebookUsernameExtractor
-  # Your code goes here...
+  def self.extract(page)
+    return if page.to_s.empty?
+    page = page.split(/(?=https?:)/).first
+    return page[/profile.php\?id=(\w+)/, 1] if page.include? 'profile.php'
+    page = page
+           .sub(%r{^https?://}, '')
+           .sub(/^([a-z\-]+\.)?facebook.com/, '')
+           .sub(%r{^/+}, '')
+           .sub(/\?.*/, '')
+           .sub(%r{^#!/}, '')
+           .sub(%r{/$}, '')
+    return if page.start_with? 'search/'
+    return page.sub('pages/', '').sub('/', '-') if page.start_with? 'pages/'
+    return page.split('/')[1] if page.start_with? 'people/'
+    return if page.include? 'facebook' # TODO: handle more cases
+    page
+  end
 end
--- test/facebook_username_extractor_test.rb
+++ test/facebook_username_extractor_test.rb
@@ -5,7 +5,27 @@ class FacebookUsernameExtractorTest < Minitest::Test
     refute_nil ::FacebookUsernameExtractor::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_doesnt_touch_plain_username
+    assert_equal 'TaylorSwift', FacebookUsernameExtractor.extract('TaylorSwift')
+  end
+
+  def test_http_url
+    assert_equal 'TaylorSwift', FacebookUsernameExtractor.extract('http://www.facebook.com/TaylorSwift')
+  end
+
+  def test_https_url
+    assert_equal 'TaylorSwift', FacebookUsernameExtractor.extract('https://www.facebook.com/TaylorSwift')
+  end
+
+  def test_trailing_slash
+    assert_equal 'TaylorSwift', FacebookUsernameExtractor.extract('https://www.facebook.com/TaylorSwift/')
+  end
+
+  def test_language_code_subdomain
+    assert_equal 'TaylorSwift', FacebookUsernameExtractor.extract('http://en-gb.facebook.com/TaylorSwift')
+  end
+
+  def test_page_id
+    assert_equal 'TaylorSwift-123456', FacebookUsernameExtractor.extract('http://www.facebook.com/pages/TaylorSwift/123456')
   end
 end
